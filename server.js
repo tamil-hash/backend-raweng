29a79b43421c8be865009c47029fd34b1e989325
tamil-kl <tamilselvan@kloudlearn.com>
2022-09-17 18:45:49 +0530

    basic setup done

---
+++ server.js
@@ -0,0 +1,11 @@
+import express from "express";
+import dotenv from "dotenv";
+
+dotenv.config({ path: ".env" });
+
+const app = express();
+
+app.listen(process.env.PORT, () => {
+  console.log(`Listening on PORT ${process.env.PORT}`);
+});
+
